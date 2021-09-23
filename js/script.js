c79e88ee448018ae7ff02c0eeb1dfd89bbaa2553
Alexander Harris <alexharris@alexanders-mbp.attlocal.net>
2021-09-23 17:41:41 -0400

    finishing js

--- js/script.js
+++ js/script.js
@@ -19,11 +19,8 @@ function getData(event) {
 
 
   $.ajax(`${mainURL}api_key=${APIKEY}&q=${query}&limit=${limit}&rating=${rating}&lang=en`).then(function (data) {
-      console.log(data)
       const randomData = randNum(0, data.data.length - 1)
-      console.log(randomData)
       const gifData = data.data[randomData].images.fixed_height.url
-      console.log(gifData)
       render(gifData)
       $input.val('')
     }),
