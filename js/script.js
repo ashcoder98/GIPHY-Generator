548118cd8b380b0e3a00c36c03f0d33540baaa36
Alex Harris <alexharrisemail@gmail.com>
2022-06-09 14:49:17 -0400

    adds styling and changes to logic

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 const mainURL = 'https://api.giphy.com/v1/gifs/search?';
 const trendingURL = 'https://api.giphy.com/v1/gifs/trending?'
 const APIKEY = '4BWZutNfTNwYrZtQgo7NooXkIwoBLlga';
-const limit = 25;
+const $limit = $('select[name="limit"]')
 const rating = 'pg';
 const $input = $('input[type="text"]');
+const $search = $('button[class="submit"]')
 const $form = $('form');
-const $random = $('form[class="random"]')
+const $random = $('button[class="trending"]')
 const $img = $('img[class="img1"]');
 const $images = $('ul[class="images"]')
+
 // Random Gif Function
 const randNum = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 };
 // On form submission
-$form.on('submit', getData)
+$search.on('click', getData)
 
 
 function getData(event) {
   event.preventDefault();
   const query = $input.val();
+  const limitnumber = $limit.val();
   if ($input.val() == ''){
     alert('Please type in a search query')
   }
@@ -26,14 +29,13 @@ function getData(event) {
 
   $.ajax(
 
-    `${mainURL}api_key=${APIKEY}&q=${query}&limit=${limit}&rating=${rating}&lang=en`
+    `${mainURL}api_key=${APIKEY}&q=${query}&limit=${limitnumber}&rating=${rating}&lang=en`
   
    
     ).then(function (data) {
-      // console.log(data)
       $.each(data.data, function (index, element) {
         console.log(element.url)
-        $images.append(`<li><img src=${element.images.fixed_height.url}></li>`)
+        $images.prepend(`<li><img src=${element.images.fixed_height.url}></li>`)
       })
       $input.val('')
 }),
@@ -50,16 +52,16 @@ function render(gifData) {
 
 /////////////////////////////
 // Random button section
-$random.on('submit', getTrendingGif)
+$random.on('click', getTrendingGif)
 
 function getTrendingGif(event) {
   event.preventDefault();
-
-  $.ajax(`${trendingURL}api_key=${APIKEY}&limit=${limit}&rating=${rating}`).then(function (data) {
+  const limitnumber = $limit.val();
+  $.ajax(`${trendingURL}api_key=${APIKEY}&limit=${limitnumber}&rating=${rating}`).then(function (data) {
       console.log(data)
       $.each(data.data, function (index, element) {
         console.log(element.url)
-        $images.append(`<li><img src=${element.images.fixed_height.url}></li>`)
+        $images.prepend(`<li><img src=${element.images.fixed_height.url}></li>`)
       })
     }),
     function (error) {
